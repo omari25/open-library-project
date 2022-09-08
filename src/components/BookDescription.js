df48a02607382884885df6728277c1fafc72742e
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-09-08 20:50:16 +0300

    Add the Book-description components

---
+++ src/components/BookDescription.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BookDescription() {
+  
+    return (
+      <div className="books-desriptions">
+        
+      </div>
+    )
+  }
+  
+  export default BookDescription;
