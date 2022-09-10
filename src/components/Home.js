c1fc55188c3b671183006fb71d274dde46525ccc
Omar Mahat <omar.mahat@student.moringaschool.com>
2022-09-10 18:24:54 +0300

    Added a paragraph

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,10 @@ function Home() {
     return (
       <div className="home">
         <h2>Welcome to Open Library</h2>
+        <center>
+          <p>Books enthusiats! Welcome to your very own highly personalized library app!</p>
+          <p>Click on the button at the bottom to view our recommendation of some entertaining books</p>
+        </center>
         <center><img src={pic} alt="Italian Trulli"/></center>
         <center><Link to={`/books`}><button>Explore</button></Link></center>
       </div>
